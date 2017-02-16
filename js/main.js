15cbf932cc725703fd5a199ff455a1001dce1137
g-mehra <gmehra@alumni.uwo.ca>
2017-02-15 20:55:25 -0500

    Assignment complete

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 
+//1
 
+$(".switch").on('click',function() {
+
+  $(".switch button").toggleClass("on off") ;
+
+  if ($(".switch button").attr('class') === "off") {
+    $("body").toggleClass('light dark') &&
+    $(".status").text("Hey, who turned off the lights?")
+  } else {
+    $("body").toggleClass('dark light')
+    &&   $(".status").text("It's so bright in here!")
+  }
+
+
+});
